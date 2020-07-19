380e9802e2bcef3f9c5a4227d16a0585a229cf1f
Lion Kapitman <l.kapitman@gmail.com>
2020-07-20 00:48:49 +0200

    Добавлены ссылки на Github и wiki, а также изменен дизайн некоторых элементов…

--- application/views/admin/pages/misc.php
+++ application/views/admin/pages/misc.php
@@ -1,12 +1,9 @@
 <div id="content">
-	<form action="" method="POST"><input type="submit" name="logout" class="button" value="Logout"></form>
-	<br /><br />
-	<h4>SEO</h4>
-		<p>Заглавие сайта</p>
-			<strong><?=$title?></strong>
-		<p>Описание сайта</p>
-			<strong><?=$description?></strong>
-			<div id="desc">
-				<p>SEO конфигурация [root\application\config\site.php]</p>
-			</div>
+	<form action="" method="POST"><input type="submit" name="logout" class="button" value="Выйти"> <a href="https://github.com/MrPHP-Team/MinecraftCMS"><input type="button" class="button" value="Git"></a> <a href="https://github.com/MrPHP-Team/MinecraftCMS/wiki"><input type="button" class="button" value="Wiki"></a></form>
+	<div id="desc">
+		Описание Сайта - <?=$description?><br>
+		<hr>
+		Заглавие сайта       - <?=$title?>
+	</div>
+	<p>Описание и название сайта можно изменить в конфигурации сайта</p>
 </div>
